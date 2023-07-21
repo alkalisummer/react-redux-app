1f2e7839c6f2a13222072861399e661ad21942d9
khkang <alkalisummer@gmail.com>
2023-07-21 13:14:03 +0900

    리덕스 스토어를 사용하여 counter 기능 추가

---
+++ src/reducers/index.tsx
@@ -0,0 +1,12 @@
+const counter = (state: number = 0, action: { type: string }) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export default counter;
